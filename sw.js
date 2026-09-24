3a8fb482ee6881c4ec3d07579144eefb32b92518
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-24 20:15:22 -0300

    Atualiza cache para destaque da mensalidade

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v10';
+const CACHE_NAME = 'baba-veteranos-v11';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
