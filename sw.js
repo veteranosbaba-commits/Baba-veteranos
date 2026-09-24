1d0d24cc51b0a29b49877ee203f0caec17c758ad
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-24 20:03:48 -0300

    Atualiza cache para confirmação de saída e exclusão

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v9';
+const CACHE_NAME = 'baba-veteranos-v10';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
