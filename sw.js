338b94aa647cd80bedc43f1eee98e1a4b5d40da1
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-24 19:20:48 -0300

    Atualiza cache para relatórios PDF por categoria

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v8';
+const CACHE_NAME = 'baba-veteranos-v9';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
