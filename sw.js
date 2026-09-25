4bccbe2d35d26483914c03aac29feeef4f864b7a
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-24 22:39:22 -0300

    Atualiza cache após remover vínculo manual por UID

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v16';
+const CACHE_NAME = 'baba-veteranos-v17';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
