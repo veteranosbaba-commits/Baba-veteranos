79584a416f9e8757b31ae4ad54201ddb6e45b894
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-25 22:43:41 -0300

    Improve public suspension wording

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v20';
+const CACHE_NAME = 'baba-veteranos-v21';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
