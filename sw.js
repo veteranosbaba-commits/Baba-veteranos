d2f63233d8200274cdb997e18f80adbe64fe76b6
veteranosbaba-commits <veteranosbaba@gmail.com>
2026-09-25 23:25:58 -0300

    Add private admin sanctions panel with sequential suspensions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - BABA VETERANOS v3 - alerta sonoro/vibração atualizado
-const CACHE_NAME = 'baba-veteranos-v21';
+const CACHE_NAME = 'baba-veteranos-v22';
 const urlsToCache = [
   '/Baba-veteranos/',
   '/Baba-veteranos/index.html',
